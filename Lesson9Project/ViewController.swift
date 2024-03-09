01e7491f5cf2787d251f5a5d15c773b09ef40198
Sandy Gomez <sgomez1029@gmail.com>
2024-03-08 17:42:10 -0800

    Lesson9Assigment (Gestures)

--- Lesson9Project/ViewController.swift
+++ Lesson9Project/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var myLabel: UILabel!
+    
+    @IBOutlet var tapHereLabel: UILabel!
+    @IBOutlet var myView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myLabel.text = "Type of gesture will appear here"
+        tapHereLabel.text = "Tap here to trigger gestures"
+        
     }
-
-
+    @IBAction func longPressGesture(_ sender: UILongPressGestureRecognizer) {
+        myView.backgroundColor = UIColor.red
+        myLabel.text = "Long press gesture"
+    }
+    
+    @IBAction func oneTapGesture(_ sender: UITapGestureRecognizer) {
+        myView.backgroundColor = UIColor.blue
+        myLabel.text = "One tap gesture"
+    }
+    
 }
 
